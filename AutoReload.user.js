c238b0874fd35f303b4b44d4683468532ef906bb
githuba <whatsmyeternity@gmail.com>
2018-09-13 21:54:48 -0700

    Update AutoReload.user.js

--- AutoReload.user.js
+++ AutoReload.user.js
@@ -12,5 +12,12 @@
 // ==/UserScript==
 
 var numMinutes = 3;
+
+/* function rand(min, max) { 
+return Math.floor(Math.random() * (max - min + 1)) + min; 
+} */
 var random = Math.floor(Math.random()*1000) + 1000;
+
+/* every 1 minute */
+/* setTimeout(function(){ location.reload(); }, 1*60*1000); */
 setTimeout(function(){ location.reload(); }, numMinutes*60*random);
