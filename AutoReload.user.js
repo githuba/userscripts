8c3692f6565a6733cae916648c81baba09018933
githuba <whatsmyeternity@gmail.com>
2018-09-13 21:57:02 -0700

    Update AutoReload.user.js

--- AutoReload.user.js
+++ AutoReload.user.js
@@ -3,7 +3,7 @@
 // @description Reload pages every X to 2X minutes
 // @author      githuba
 // @namespace   https://github.com/githuba/userscripts
-// @version     0.1.1
+// @version     0.1
 // @downloadURL https://raw.githubusercontent.com/githuba/userscripts/master/AutoReload.user.js
 // @include     http://club.tgfcer.com/*
 // @include     https://steamcn.com/forum.php?mod=forumdisplay&fid=234&filter=author&orderby=dateline
