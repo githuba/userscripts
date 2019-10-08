d859b5ea365a381216daba7cde2c67c9dd31e848
githuba <whatsmyeternity@gmail.com>
2019-10-08 01:51:43 -0700

    Update AutoReload.user.js

--- AutoReload.user.js
+++ AutoReload.user.js
@@ -4,7 +4,7 @@
 // @author      githuba
 // @namespace   https://github.com/githuba/userscripts
 // @supportURL  https://github.com/githuba/userscripts/issues
-// @version     2.2
+// @version     2.3
 // @downloadURL https://raw.githubusercontent.com/githuba/userscripts/master/AutoReload.user.js
 // @include     https://*.tgfcer.com/*
 // @include     http://bbs.weibufengge.com/*
@@ -12,6 +12,7 @@
 // @include     https://steamcn.com/forum.php?mod=guide*
 // @include     https://na.alienwarearena.com/
 // @include     https://www.steamgifts.com/*
+// @include     https://www.hi-pda.com/forum/forumdisplay.php?*
 // @run-at document-end
 // ==/UserScript==
 
